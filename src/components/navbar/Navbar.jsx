676b9027c2c40f9b65236cce96d647290de792c1
Julianah Babatunde <babatundejulianah03@gmail.com>
2023-05-10 14:55:48 +0100

    starting-amazon

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+        <nav>
+            <a href="https://www.amazon.com/ref=nav_logo"></a>
+        </nav>
+    </div>
+  )
+}
+
+export default Navbar
